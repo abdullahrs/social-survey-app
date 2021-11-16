2d1ee480be9e99754d19bf1ad424cdb4810a757c
abdullahrs <abdullahrsimsek@gmail.com>
2021-11-16 12:55:17 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/anket/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.anket
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/api_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+
+  test('[Test] Sign-up', () {
+  });
+  test('[Test] Log-in', () {
+  });
+
+  test('[Test] Log-out', () {
+  });
+
+  test('[Test] Forgot Password', () {
+  });
+
+  test('[Test] Get survey categories', () {
+  });
+
+  test('[Test] Get popular surveys', () {
+  });
+
+  test('[Test] Get surveys participated in', () {
+  });
+
+  test('[Test] Get searched surveys', () {
+  });
+
+  test('[Test] Post survey', () {
+  });
+
+  test('[Test] Get survey results', () {
+  });
+}
