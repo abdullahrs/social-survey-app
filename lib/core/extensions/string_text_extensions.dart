f50afa33e92eff7e5836bff2e4c14b34204f97cd
abdullahrs <abdullahrsimsek@gmail.com>
2022-05-17 16:39:52 +0300

    fix: string extension bug fix

--- lib/core/extensions/string_text_extensions.dart
+++ lib/core/extensions/string_text_extensions.dart
@@ -3,6 +3,8 @@ extension StringExtendedExtension on String {
     List<String> words = split(' ');
     List<String> temp = [];
     for (String item in words) {
+      if (item.isEmpty) continue;
+      if (item.length < 2) continue;
       temp.add(item[0].toUpperCase() + item.substring(1));
     }
     return temp.join(' ');
